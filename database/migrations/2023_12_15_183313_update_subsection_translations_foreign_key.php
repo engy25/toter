76afefc56cd2ce913577d90fb85d48e8611f45ae
engy25 <engymohammed156@gmail.com>
2024-01-28 16:42:19 +0200

    changes in user api

--- database/migrations/2023_12_15_183313_update_subsection_translations_foreign_key.php
+++ database/migrations/2023_12_15_183313_update_subsection_translations_foreign_key.php
@@ -4,39 +4,38 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-      Schema::table('subsection_translations', function (Blueprint $table) {
-        $table->dropForeign(['sub_section_id']);
+return new class extends Migration {
+  /**
+   * Run the migrations.
+   *
+   * @return void
+   */
+  public function up()
+  {
+    Schema::table('subsection_translations', function (Blueprint $table) {
+      // $table->dropForeign(['sub_section_id']);
 
-        $table->foreign('sub_section_id')
-            ->references('id')->on('subsections')
-            ->onDelete('cascade');
+      // $table->foreign('sub_section_id')
+      //   ->references('id')->on('subsections')
+      //   ->onDelete('cascade');
     });
 
     Schema::table('offers', function (Blueprint $table) {
-      $table->dropForeign(['subsection_id']);
+      // $table->dropForeign(['subsection_id']);
 
-      $table->foreign('subsection_id')
-          ->references('id')->on('subsections')
-          ->onDelete('cascade');
-  });
-    }
+      // $table->foreign('subsection_id')
+      //   ->references('id')->on('subsections')
+      //   ->onDelete('cascade');
+    });
+  }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        //
-    }
+  /**
+   * Reverse the migrations.
+   *
+   * @return void
+   */
+  public function down()
+  {
+    //
+  }
 };
